5dcbd8e2da9e3c4abc7f1841facd4a392ec550e0
Finn <finn@finn.io>
2017-07-05 13:56:34 -0700

    Initial callcenter queue listing

---
+++ actions/callcenter-status.php
@@ -0,0 +1,8 @@
+<?php
+$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+if (!$fp) {die("Failed to connect");}
+
+
+$response = event_socket_request($fp, "api callcenter_config queue list members ".$_POST['queue_name']);
+
+echo json_encode(array("result" => explode("\n", $response)));
