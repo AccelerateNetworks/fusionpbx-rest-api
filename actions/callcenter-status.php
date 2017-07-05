4b95d9be653535415ad8bea1117e5f176bb1ed30
Finn <finn@finn.io>
2017-07-05 16:17:35 -0700

    Actually parse out the status

--- actions/callcenter-status.php
+++ actions/callcenter-status.php
@@ -5,4 +5,24 @@ if (!$fp) {die("Failed to connect");}
 
 $response = event_socket_request($fp, "api callcenter_config queue list members ".$_POST['queue']);
 
-echo json_encode(array("result" => explode("\n", $response)));
+$lines = explode("\n", trim($response));
+
+$status = array_pop($lines);
+if($status != "+OK") {
+  die(json_encode(array("success" => False, "reason" => $status)));
+}
+
+$keys = explode("|", array_shift($lines));
+
+$out = array();
+
+foreach($lines as $orig_line) {
+  $line = array();
+  foreach(explode("|", $orig_line) as $key => $value) {
+    $line[$keys[$key]] = $value;
+  }
+  $out[] = $line;
+}
+
+
+echo json_encode($out);
