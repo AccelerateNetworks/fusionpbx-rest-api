ca6214b08e4d328d89a9344ffb531954d1bd7143
Finn <finn@finn.io>
2017-07-05 13:57:33 -0700

    Fix POST name

--- actions/callcenter-status.php
+++ actions/callcenter-status.php
@@ -3,6 +3,6 @@ $fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event
 if (!$fp) {die("Failed to connect");}
 
 
-$response = event_socket_request($fp, "api callcenter_config queue list members ".$_POST['queue_name']);
+$response = event_socket_request($fp, "api callcenter_config queue list members ".$_POST['queue']);
 
 echo json_encode(array("result" => explode("\n", $response)));
