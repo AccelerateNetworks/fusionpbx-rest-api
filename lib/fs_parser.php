1914d0e0919a986f89142b25fd69c90bbb88e158
Finn <finn@finn.io>
2017-07-05 21:38:12 -0700

    less bad error handling

--- lib/fs_parser.php
+++ lib/fs_parser.php
@@ -2,14 +2,21 @@
 function parse_fs($command) {
   $fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
   if (!$fp) {
-    die(json_encode(array("success" => False, "reason" => "Failed to connect to event socket")));
+    die(json_encode(array("success" => false, "reason" => "Failed to connect to event socket")));
   }
 
   $response = event_socket_request($fp, $command);
 
-  $lines = explode("\n", $response);
+  $lines = explode("\n", trim($response));
+
+  $status = array_pop($lines);
+
+  if($status != "+OK") {
+    die(json_encode(array("success" => false, "reason" => $status)));
+  }
 
   $keys = explode("|", array_shift($lines));
+
   $out = array();
   foreach($lines as $orig_line) {
     $line = array();
