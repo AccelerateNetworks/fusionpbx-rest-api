a6de2dd1fc0c57f0b32534ed7164a7827457f475
Finn <finn@finn.io>
2017-07-05 23:43:00 -0700

    I may have missunderstood the purpose of in_array

--- actions/callcenter-queuesize.php
+++ actions/callcenter-queuesize.php
@@ -4,7 +4,7 @@ $counts = array();
 $users = parse_fs("api callcenter_config queue list members ".$_POST['queue']);
 foreach($users as $user) {
   $state = $user['state'];
-  if(!in_array($state, $counts)) {
+  if(!isset($counts[$state])) {
     $counts[$state] = 0;
   }
   $counts[$state]++;
