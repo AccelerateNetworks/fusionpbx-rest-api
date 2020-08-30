d97b5dd2a9f537d7fb0951e15f903d1ca9144a37
Dan Ryan <dan@acceleratenetworks.com>
2020-08-30 00:44:39 -0700

    Add originate-playback-file, update readme

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 This README is lacking. For now. This is an app for [FusionPBX](http://www.fusionpbx.com/).
 
 # Install
-To install it, clone into fusionpbx's `app/` folder and run the menu upgrades (Advanced->Upgrade,
-  check off Menu Defaults and hit Execute). Note that FusionPBX seems to prefer lower case naming
-  with underscores, so when cloning be sure to clone to or rename the folder `rest_api`.
+To install it, clone into fusionpbx's `app/` folder and run the menu upgrades (Log into the web
+  interface, Advanced -> Upgrade, check off Menu Defaults and hit Execute).
+
+Define a settings.json file containing the authorized API keys along with the gateway and any
+prefix needed for calls to route if required.
+
+Example settings.json with multiple API tokens using gateway that needs no prefix to make calls:
+```
+{
+  "tokens": [
+    "uoF3SoonHuiw9eewahKoh1ooFoov9eiFOhjiey8o",
+    "cc5e74ea-6b94-41e8-b743-56abc008d934",
+    "random-applications-api-key"
+  ],
+  "originate": {
+    "prefix": "",
+    "gateway": "cc5e74ea-6b94-41e8-b743-56abc008d934"
+  }
+}
+```
---
+++ actions/originate-playback-file.php
@@ -0,0 +1,24 @@
+<?php
+$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+if (!$fp) {die("Failed to connect");}
+
+$to = "{ignore_early_media=true,originate_timeout=30,effective_caller_id_number=".$_POST['cid']."}sofia/gateway/".$settings['gateway']."/".$settings['prefix'].intval($_POST['to']);
+if (substr($_POST['file'], 0, 28) === "/usr/share/freeswitch/sounds") {
+  $file = $_POST['file'];
+} elseif(substr($_POST['file'], 0, 4) === "http") {
+  $file = $_POST['file'];
+} else {
+  $file = "/usr/share/freeswitch/sounds/en/us/callie/misc/8000/ivr-congratulations_you_pressed_star.wav"
+}
+
+$command = "api originate ".$to." '&playback(".$file.")' XML default LEGIT_CALL ".$_POST['cid'];
+$response = event_socket_request($fp, $command);
+$response = explode(" ", $response, 2);
+$success = False;
+$call_uuid = NULL;
+if($response[0] == "+OK") {
+  $success = True;
+  $call_uuid = trim($response[1]);
+}
+
+echo json_encode(array("command" => $command, "success"=>$success, "call_uuid"=>$call_uuid));
